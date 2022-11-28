256c8b7a1aa2be38ae4b495930064ee466c6593e
Heli Parviainen <heli.s.parviainen@helsinki.fi>
2022-11-28 22:40:20 +0200

    TDD kahden eri tuotteen lisäämisen jälkeen ostoskorin hinta on ostosten yhteishinta

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -25,6 +25,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(self.kurkku)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
 
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korin_hinta_on_tuotteiden_hinta(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.kurkku)
+        self.assertEqual(self.kori.hinta(), 19)
+
     def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
         self.kori.lisaa_tuote(self.maito)
         self.kori.lisaa_tuote(self.maito)
