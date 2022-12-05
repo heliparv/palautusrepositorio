aa8537ed0303d679f7cffdd54a78f8531df84a88
Heli Parviainen <heli.s.parviainen@helsinki.fi>
2022-12-05 16:08:55 +0200

    Simplify how score returned

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -17,32 +17,16 @@ class TennisGame:
         temp_score = 0
 
         if self.player1_score == self.player2_score:
-            score = self.score_equal()
+            return self.score_equal()
 
         elif max(self.player1_score, self.player2_score) >= 4:
             if self.player1_score > self.player2_score:
-                self.scores_high("player1")
+                return self.scores_high("player1")
             else:
-                self.scores_high("player2")
+                return self.scores_high("player2")
             
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.player1_score
-                else:
-                    score = score + "-"
-                    temp_score = self.player2_score
-
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
-
-        return score
+            return f"{self.score_names[self.player1_score]}-{self.score_names[self.player2_score]}"
 
     def score_equal(self):
         if self.player1_score < 3:
