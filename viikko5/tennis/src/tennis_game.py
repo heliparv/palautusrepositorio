dfe117e00f78bfa6f9c234e8d35295e1aff9f978
Heli Parviainen <heli.s.parviainen@helsinki.fi>
2022-12-05 15:59:15 +0200

    Rename player scores

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -2,24 +2,25 @@ class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1_score = 0
+        self.player2_score = 0
         self.score_names = {0:"Love", 1:"Fifteen", 2:"Thirty", 3:"Forty"}
 
     def won_point(self, player_name):
         if player_name == self.player1_name:
-            self.m_score1 += 1
+            self.player1_score += 1
         else:
-            self.m_score2 += 1
+            self.player2_score += 1
 
     def get_score(self):
         score = ""
         temp_score = 0
 
-        if self.m_score1 == self.m_score2:
+        if self.player1_score == self.player2_score:
             score = self.score_equal()
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
+
+        elif max(self.player1_score, self.player2_score) >= 4:
+            minus_result = self.player1_score - self. player2_score
 
             if minus_result == 1:
                 score = "Advantage player1"
@@ -32,10 +33,10 @@ class TennisGame:
         else:
             for i in range(1, 3):
                 if i == 1:
-                    temp_score = self.m_score1
+                    temp_score = self.player1_score
                 else:
                     score = score + "-"
-                    temp_score = self.m_score2
+                    temp_score = self.player2_score
 
                 if temp_score == 0:
                     score = score + "Love"
@@ -49,7 +50,7 @@ class TennisGame:
         return score
 
     def score_equal(self):
-        if self.m_score1 < 3:
-            return f"{self.score_names[self.m_score1]}-All"
+        if self.player1_score < 3:
+            return f"{self.score_names[self.player1_score]}-All"
         else:
             return "Deuce"
